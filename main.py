e107c6013f71a2601ec2f806616c0d9dc2b5446d
Matymatyk <matyas.stoch@gmail.com>
2026-03-16 10:24:13 +0100

    fix: repair broken __main__ string in main.py

--- main.py
+++ main.py
@@ -25,6 +25,5 @@ app.include_router(auth_router)
 app.include_router(dashboard_router)
 
 
-if __name__ == "__mai
-n__":
+if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
